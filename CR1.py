7b759a0201423447b2690e9b08475c68d310ee34
fintanduff <146032642+fintanduff@users.noreply.github.com>
2025-02-01 16:12:50 +0000

    Create CR1.py

---
+++ CR1.py
@@ -0,0 +1,8 @@
+def every_nth(n, p):
+    if p % 2 != 0 or n > p // 2:
+        raise ValueError("Invalid input: p must be even and n must be ≤ p/2")
+      #This ensures that only valid numbers n and p may be used
+    
+    start = p // 2  # First number in range
+    return list(range(start, p + 1, n))  # Generate list using range()
+# 
